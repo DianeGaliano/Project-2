4e57ed924fd99c77839cd55bbc0164a0c57c9ba4
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-11-06 12:06:10 -0500

    created form for customer info

--- public/js/test.js
+++ public/js/test.js
@@ -1,15 +1,16 @@
 console.log("yo");
 
 function sendColor() {
-  fetch("/api/new/testColor", {
+  fetch("/api/carInfoRoutes", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      color: document.getElementById("name").value,
+      name: document.getElementById("name").value,
     }),
   });
 }
+
 
 document.getElementById("btn").addEventListener("click", sendColor);
